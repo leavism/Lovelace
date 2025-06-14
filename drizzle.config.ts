f5db805d7d6e2471a88a87a9cbe3755c47af5d61
Huy Dang <ghuydang@gmail.com>
2025-06-14 08:45:36 -0700

    Update drizzle.config.ts with db url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: 'mysql',
   schema: './src/db/schema/schema.ts',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!, 
+  },
 })
